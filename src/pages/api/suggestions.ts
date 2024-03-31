a181a03930133aff63f3ebac85c5bb9b1ddd5471
Zeyu Yao <novodoodle@gmail.com>
2024-03-31 18:10:55 +0800

    Add form fields to Home component for further customization

--- src/pages/api/suggestions.ts
+++ src/pages/api/suggestions.ts
@@ -7,6 +7,81 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+// Vocab levels and tones
+const vocabLevels = ["Graduate", "High School (K-12)"];
+const tones = ["Friendly", "Neutral", "Formal"];
+
+var instructions = `
+[User Configuration]
+    Vocabulary Level: ${vocabLevels[0]}
+    Tone: ${tones[0]}
+
+[Overall Rules to Follow]
+    1. Analyze text for structure, style, and coherence.
+    2. Offer constructive feedback aimed at improving clarity, vocabulary, grammatical accuracy, and flow.
+    3. Provide specific, actionable suggestions for each identified area of improvement.
+    4. Include examples to illustrate suggested improvements.
+    5. Use any provided question to focus and tie your feedback to the main discussion or answer attempted by the text.
+    6. When applicable, ensure feedback aligns with criteria-specific strands, particularly for IB assessments, focusing on meeting these strands.
+    7. Focus feedback on elevating the quality of the writing according to established linguistic standards.
+    8. Give feedback in a clear, concise, and numbered list, avoiding general suggestions.
+    9. Highlight specific examples where improvement is needed, including a direct example for clarity.
+    10. Act as a guide, similar to a teacher's approach, pointing out precise areas for improvement and how to achieve it.
+    11. Tailor feedback to the user's specified Vocabulary Level and Tone.
+    12. Ensure the analysis is accessible and understandable at the user's vocabulary level.
+    13. Avoid overly technical jargon unless it is within the user's configuration for Vocabulary Level.
+    14. Communicate only in English, translating and adjusting non-English text to match the user's configuration
+
+[Functions]
+    [say, Args: text]
+        [BEGIN]
+            Strictly convey the <text>, filling in <...> with the appropriate feedback, suggestions, and examples.
+        [END]
+    [sep]
+        [BEGIN]
+            say ---
+        [END]
+
+[Example - with question]
+    Question: How does globalization affect local cultures?
+    Text: Globalization has a significant impact on local cultures, often leading to the blending of cultures and sometimes even resulting in cultural homogenization. While this can lead to a richer global culture, it also poses a threat to the uniqueness of local cultures.
+
+    Feedback:
+    1. Clarify how globalization leads to cultural blending. For example: "Through the widespread availability of international media, local populations are exposed to foreign cultures..."
+    2. Provide examples of cultural homogenization. For example: "The global popularity of fast-food chains like McDonald's illustrates how local culinary traditions can be overshadowed..."
+    3. Discuss the implications for local uniqueness. For example: "This cultural convergence might dilute the distinctiveness of local traditions, languages, and arts..."
+
+[Example - with IB criteria]
+    Question: How does globalization affect local cultures?
+    Strands:
+    i. consistently uses a wide range of terminology effectively
+    ii. demonstrates excellent knowledge and understanding of content and concepts through thorough, accurate descriptions, explanations and examples.
+    Text: Globalization significantly impacts local cultures, often leading to the blending of cultures and sometimes even resulting in cultural homogenization. While this can lead to a richer global culture, it also poses a threat to the uniqueness of local cultures.
+    
+    Feedback:
+    1. To align with Criterion A (i), utilize a broader range of terminology effectively. For example: "Discuss 'cultural diffusion' and 'global homogenization' to provide a nuanced understanding of globalization's impacts..."
+    2. To meet Criterion A (ii), enhance your knowledge and understanding through thorough, accurate descriptions and examples. For example: "Delve into 'cultural assimilation' processes, using real-world examples like the influence of global media on local traditions..."
+
+[Example - without question or criteria]
+    Text: The research methodology adopted in the study was both qualitative and quantitative, aiming to gather a broad spectrum of data from various sources. This approach enabled the triangulation of data, enhancing the reliability and validity of the research findings.
+
+    Feedback:
+    1. Specify the types of qualitative and quantitative methods used. For example: "We employed structured interviews and surveys..."
+    2. Mention specific sources of data. For example: "Data was collected from three hospitals and two online surveys..."
+    3. Explain how triangulation enhances reliability and validity. For example: "Triangulation, by combining multiple methods, ensures that our findings are well-rounded and robust..."
+
+[Personalization Options]
+    Vocabulary Level:
+        ["Graduate", "High School (K-12)"]
+
+    Tone:
+        ["Friendly", "Neutral", "Formal"]
+
+[Function Rules]
+    1. Act as if you are executing code.
+    2. Do not say: [INSTRUCTIONS], [BEGIN], [END], [IF], [ENDIF], [ELSEIF]
+`;
+
 const rateLimit = (
   req: NextApiRequest,
   res: NextApiResponse,
@@ -36,21 +111,35 @@ export default async function handler(
 
   if (req.method === "POST") {
     try {
-      const { text } = req.body;
+      const { text, question, strands } = req.body;
+
+      let input = "";
+
+      if (question) {
+        input += `Question: ${question}\n`;
+      }
+
+      if (strands) {
+        input += `Strands:\n`;
+        for (let i = 0; i < strands.length; i++) {
+          input += `${i + 1}. ${strands[i]}\n`;
+        }
+      }
+
+      input += `Text: ${text}`;
+
       const response = await openai.chat.completions.create({
         messages: [
           {
             role: "system",
-            content:
-              "You are an advanced digital assistant designed to provide constructive feedback on text. Your primary function is to analyze the structure, style, and coherence of written material to identify areas for improvement. Focus on enhancing clarity, enriching vocabulary, ensuring grammatical accuracy, and refining the overall flow of the writing. When evaluating text against established linguistic standards, aim to elevate the quality of the content. Remember, your role is not to rewrite or enhance the text directly, but rather to offer specific, actionable suggestions for improvement. Provide these suggestions in a clear, concise, and numbered list, with each suggestion on a new line. General suggestions are not appreciated, and it's not necessary to suggest improvements for every area. Emphasize specific examples on how to improve if necessary but do not provide the complete enhancement. Imagine you're a teacher giving students feedback, pointing out precisely where they can improve and how.",
+            content: instructions,
           },
           {
             role: "user",
-            content: `Please offer suggestions to improve the text in a numbered list: \n\n${text}`,
+            content: input,
           },
         ],
         model: "gpt-3.5-turbo",
-        max_tokens: 150,
         temperature: 0.7,
       });
 
@@ -67,7 +156,6 @@ export default async function handler(
   }
 }
 
-// Make sure to initialize the global object somewhere in your application
 if (!(global as any).rateLimit) {
   (global as any).rateLimit = {};
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,31 +4,49 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [question, setQuestion] = useState<string>('');
+  const [strands, setStrands] = useState<string[]>([]);
   const [text, setText] = useState<string>('');
+
+  const [loading, setLoading] = useState<boolean>(false);
   const [suggestionsList, setSuggestionsList] = useState<string[]>([]);
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  const handleQuestionChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setQuestion(e.target.value);
+  }
+
+  const handleStrandsChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setStrands(e.target.value.split('\n'));
+  }
+
+  const handleTextChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+  };
+
+  const handleTextSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    if (!text) {
+      return;
+    }
+
+    setLoading(true);
 
     const response = await fetch('/api/suggestions', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ text }),
-    });
+      body: JSON.stringify({ text, question, strands }),
+    })
 
     const data = await response.json();
+
+    setLoading(false);
 
     // Split and trim the suggestions if they're in a single string format
     const suggestionsArray = data.suggestions.split(/\d+\./).slice(1).map((s: string) => s.trim());
     setSuggestionsList(suggestionsArray);
-
-    console.log(suggestionsArray);
-  };
-
-  const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setText(e.target.value);
   };
 
   const suggestionContainerClass = suggestionsList.length
@@ -40,29 +58,48 @@ export default function Home() {
       className={`flex h-screen items-center justify-center p-4 ${inter.className}`}
       style={{ fontFamily: '"Inter", sans-serif' }}
     >
-      <div className="flex w-full max-w-4xl h-3/4 flex-col">
+      <div className="flex w-full max-w-4xl h-1/2 flex-col">
         <h1 className="text-3xl text-center mb-4">This is <span className="font-bold">Yapper.</span> It helps you learn, fast. 🗣️🗣️🗣️</h1>
         <p className="text-lg text-center mb-5">Enter your text below to receive suggestions on how to improve its clarity, style, and overall quality.</p>
-        <div className="flex flex-1">
+        <div className="flex flex-1 h-full">
           <div className="flex-1 flex flex-col justify-center p-5">
-            <form className="relative h-full" onSubmit={handleSubmit}>
+            <form className="relative h-1/6 mb-5">
               <textarea
                 className="w-full p-4 h-full text-black bg-white rounded-lg shadow focus:outline-none focus:ring focus:ring-opacity-50 resize-none"
                 rows={10}
-                placeholder="Enter text here..."
+                placeholder="Enter the question here... (optional)"
+                value={question}
+                onChange={handleQuestionChange}
+              ></textarea>
+            </form>
+            <form className="relative h-1/4 mb-5">
+              <textarea
+                className="w-full p-4 h-full text-black bg-white rounded-lg shadow focus:outline-none focus:ring focus:ring-opacity-50 resize-none"
+                rows={10}
+                placeholder="Enter the 7-8 criteria-specific strands here... (optional)"
+                value={strands}
+                onChange={handleStrandsChange}
+              ></textarea>
+            </form>
+            <form className="relative h-1/2" onSubmit={handleTextSubmit}>
+              <textarea
+                className="w-full p-4 h-full text-black bg-white rounded-lg shadow focus:outline-none focus:ring focus:ring-opacity-50 resize-none"
+                rows={10}
+                placeholder="Enter your text here..."
                 value={text}
-                onChange={handleChange}
+                onChange={handleTextChange}
               ></textarea>
               <button
                 type="submit"
-                className="absolute bottom-0 left-0 mb-5 ml-4 px-4 py-2 text-white bg-black rounded opacity-75 hover:opacity-100 focus:outline-none focus:ring focus:ring-opacity-50 transition-opacity"
+                disabled={loading}
+                className={`absolute bottom-0 left-0 mb-5 ml-4 px-4 py-2 text-white ${loading ? 'bg-gray-500' : 'bg-blue-800'} rounded opacity-75 hover:opacity-100 focus:outline-none focus:ring focus:ring-opacity-50 transition-opacity`}
               >
-                Submit
+                {loading ? 'Analyzing...' : 'Analyze'}
               </button>
             </form>
           </div>
-          <div className="flex-1 flex flex-col justify-center p-5 text-gray-500">
-            <div className={`p-4 bg-white rounded-lg shadow ${suggestionContainerClass}`}>
+          <div className="flex-1 flex flex-col justify-center p-5 text-white">
+            <div className={`p-4 bg-blue-600 rounded-lg h-full shadow ${suggestionContainerClass}`}>
               {suggestionsList.length > 0 ? (
                 <ol className="list-decimal list-inside">
                   {suggestionsList.map((suggestion, index) => (
